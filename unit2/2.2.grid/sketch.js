a9aee6cf188a98c02846568b811a4704ff0dec48
seanslev <sslevin@mail.umw.edu>
2024-09-29 19:30:29 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -8,4 +8,14 @@ function setup() {
 
 function draw() {
   background(220);
+
+  translate(100,100);
+  for (let x = 0; x < 8; x++){
+    for (let y = 0; y < 8; y++){
+        push();
+        translate(x * 100, y * 100);
+        ellipse(0,0,100,100);
+        pop();
+   }
+}
 }
