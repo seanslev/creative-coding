57c8f688ee30285b4608b5838357b729227df489
seanslev <sslevin@mail.umw.edu>
2024-10-02 23:05:58 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -4,6 +4,7 @@ function setup() {
 
   // disable animation
   noLoop();
+  frameRate(5);
 }
 
 function draw() {
@@ -15,17 +16,21 @@ function draw() {
         push();
         translate(x * 100, y * 100);
         //setting random values for RBG for 3 different things
-        let r = random(255);
-        let b = random(255);
-        let g = random(255);
+        let r = random([129,56,33,29,9,42,]);
+        let b = random([66,53,31,28,8,90,]);
+        let g = random([245,242,153,102,87,181]);
 
-        let r2 = random(255);
-        let b2 = random(255);
-        let g2 = random(255);
+        let r2 = random([129,56,33,29,9,42]);
+        let b2 = random([66,53,31,28,8,90]);
+        let g2 = random([245,242,153,102,87,181]);
 
-        let r3 = random(255);
-        let b3 = random(255);
-        let g3 = random(255);
+        let r3 = random([129,56,33,29,9,42]);
+        let b3 = random([66,53,31,28,8,90]);
+        let g3 = random([245,242,153,102,87,181]);
+
+        //chooses a random shape each loop to print inside our grid
+        let shapeRandom = random([rect, ellipse, square, quad, line, triangle]);
+
 
         //fill the squares with a random rbg color
         fill(r,b,g);
@@ -38,18 +43,26 @@ function draw() {
         //previous random color picker
         //stroke(random(["lime","blue","purple","red","orange","yellow","pink","teal"]));
 
-        strokeWeight(random(10));
+        strokeWeight(4);
 
-        rect(0,0,100,100);
-        //chooses a random shape each loop to print inside our grid
-        let shapeRandom = random([ellipse, square, quad, line, triangle]);
+        //if (shapeRandom == ellipse) {
+        //  shapeRandom(0,0,50,50);
+        //}
+        if (shapeRandom != ellipse) {
+          shapeRandom(0,0,100,100);
+        }
+        if (shapeRandom == triangle) {
+          shapeRandom(0,0,50,100,100,0);
+        }
+        
+        
         //determines random values for x and y 
         let x2 = random(50);
         let y2 = random(50);
         //prints a shape with random width, height, color, fill, and thickness
         fill(r3,b3,g3);
 
-        shapeRandom(random(50),random(50), x2, y2);
+        //shapeRandom(random(50),random(50), x2, y2);
 
         pop();
    }
