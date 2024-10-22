0bd19ab758903a6eba603c9ac1e2ce146fceb6df
seanslev <sslevin@mail.umw.edu>
2024-10-22 12:06:56 -0400

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -4,8 +4,8 @@ let x2 = 0;
 let y2 = 50;
 let xspeed = 5;
 let yspeed = 5;
-
-
+let img;
+let collisionCount;
 // Load the image.
 //function preload() {
  // samsmile = loadImage('output-onlinepngtools.png');
@@ -35,7 +35,8 @@ function draw(){
       let b = random(255);
       let g = random(255);
       fill(r,g,b);
-      
+      img = samSad;
+      collisionCount = frameCount;
   }
   if (y < 0 | y > height-50){
       yspeed = yspeed * -1; // reverse the x direction
@@ -43,6 +44,8 @@ function draw(){
       let b = random(255);
       let g = random(255);
       fill(r,g,b);
+      img = samSad;
+      collisionCount = frameCount;
       
   }
   
@@ -50,11 +53,17 @@ function draw(){
 
   y = y + yspeed; // iterate y
 
+  
 
   //let angle = frameCount * 0.02;
   //rotate(angle);
   // draw a circle
-  image(samSmile,x,y,50,50);
+  image(img,x,y,50,50);
+
+  if (collisionCount+40 < frameCount){
+    img= samSmile;
+  }
+  
   //rect(x,y,50,50);
   //ctx.drawImage(samsmile,x,y);
   //image(samsmile,x, y, 50,50);
