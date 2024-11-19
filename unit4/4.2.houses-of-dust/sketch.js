17b92f2b288a123ed85f83587daa9b9c7b7544d7
seanslev <sslevin@mail.umw.edu>
2024-11-18 22:44:20 -0500

    Update sketch.js

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,28 +1,81 @@
 let poem = '';
 
 let grammar = tracery.createGrammar({
-  "material": [
-    "SAND",
-    "DUST",
-    "LEAVES",
-    "PAPER",
-    "TIN",
-    "ROOTS",
-    "BRICKS",
-    "BROKEN DISHES",
-    "WOOD",
-    "STRAW",
-    "WEEDS"
-  ],
-  "light":[
-    "USING CANDLES",
-    "USING ELECTRICITY",
-    "USING OIL LAMPS",
-  ],
-  "origin": "A HOUSE OF #material# \n #light# "
+    "material": [
+        "WOOL",
+        "DUST",
+        "MOSS",
+        "PAPER",
+        "COPPER",
+        "ROOTS",
+        "FOIL",
+        "BALLOONS",
+        "WOOD",
+        "STRAW",
+        "WEEDS",
+        "STONE",
+        "GLASS",
+        "CLAY",
+        "METAL",
+        "FIBER"
+    ],
+    "place": [
+        "IN A DESERT",
+        "IN A FOREST",
+        "A CITY",
+        "NEAR AN ABANDONED CEMETARY",
+        "SURROUNDED BY A GARDEN",
+        "ON A SHORE",
+        "NEXT TO A COTTAGE",
+        "IN A GHOST TOWN",
+        "ON A MOUNTAINTOP",
+        "ON AN ISLAND",
+        "IN A VALLEY",
+        "NEAR A BRIDGE",
+        "BESIDE A CAVE",
+        "ON AN OLD ROAD",
+        "OVER A HILL"
+    ],
+    "light_source": [
+        "CANDLES",
+        "OIL LAMPS",
+        "ELECTRICITY",
+        "MOONLIGHT",
+        "SUNLIGHT",
+        "FIRELIGHT",
+        "TORCHES",
+        "LANTERNS",
+        "GLOWWORMS",
+        "STARS",
+        "WILDFIRE",
+        "NEON LIGHT",
+        "SUNSET",
+        "TWILIGHT",
+        "FIREWORKS",
+        "LIGHTNING"
+    ],
+    "inhabitants": [
+        "GHOSTS",
+        "WIND",
+        "MEMORIES",
+        "ANIMALS",
+        "SHADOWS",
+        "SPIRITS",
+        "HUMANS",
+        "FLIES",
+        "WOLVES",
+        "BIRDS",
+        "LIZARDS",
+        "TREES",
+        "BATS",
+        "MICE",
+        "FAIRIES",
+        "NOSTALGIA"
+    ],
+    "origin": "A HOUSE OF #material#\n#place#\nILLUMINATED WITH #light_source#\nINHABITED BY #inhabitants#"
 });
 
-poem = grammar.flatten("#origin#");
+poem = grammar.flatten("#origin#");        
 
 
 function setup() {
